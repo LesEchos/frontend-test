373a6bd640c4f673eec6e710918520d875231263
Gary FELICITE-PRUDENT <usualpro@gmail.com>
2024-04-07 12:12:35 +0200

    update newsletter CTA on user update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
+import { create } from "zustand";
+
 import {
   USER_WITH_ONE_SUBSCRIPTION,
   USER_WITHOUT_SUBSCRIPTION,
@@ -8,15 +10,6 @@ import {
 } from "../mocks/user";
 
 import { NEWSLETTER_ITEMS } from "../mocks/newsletters";
-
-interface INewsLetter {
-  id: string;
-  image: string;
-  description: string;
-  title: string;
-  site: string;
-  subscriptions: string[];
-}
 
 interface IUser {
   id: string;
@@ -27,6 +20,28 @@ interface IUser {
   subscriptions: string[];
 }
 
+interface PageState {
+  currentUser: string;
+  updateCurrentUser: (user: string) => void;
+}
+
+const usePageStore = create<PageState>((set) => ({
+  currentUser: "USER_WITH_MULTIPLE_SUBSCRIPTION",
+  updateCurrentUser: (user) =>
+    set({
+      currentUser: user,
+    }),
+}));
+
+interface INewsLetter {
+  id: string;
+  image: string;
+  description: string;
+  title: string;
+  site: string;
+  subscriptions: string[];
+}
+
 const Users = {
   USER_WITH_ONE_SUBSCRIPTION,
   USER_WITHOUT_SUBSCRIPTION,
@@ -34,12 +49,24 @@ const Users = {
 };
 
 const Block = ({ image, title, description, subscriptions }: INewsLetter) => {
+  const { currentUser } = usePageStore((state) => state);
+  const currentUserSubscriptions = Users[currentUser].subscriptions as [];
+
+  const userHasSubscriptions = subscriptions.every((e) =>
+    currentUserSubscriptions.includes(e)
+  );
+
   return (
     <div>
       <img {...{ title }} src={image} />
       <h2>{title}</h2>
       <p>{description}</p>
-      <button></button>
+
+      <button>
+        {subscriptions.length === 0 || !userHasSubscriptions
+          ? "abonne toi"
+          : "S'inscrire"}
+      </button>
     </div>
   );
 };
@@ -47,25 +74,25 @@ const Block = ({ image, title, description, subscriptions }: INewsLetter) => {
 const Sites = [...new Set(NEWSLETTER_ITEMS.map((item) => item.site))];
 
 const Loop = () =>
-  Sites.map((e, i) => (
-    <Fragment key={`Loop_${i}`}>
-      <h1>{e}</h1>
-      {NEWSLETTER_ITEMS.filter((c) => c.site === e).map((d, l) => {
-        return <Block key={`Block_${l}`} {...d} />;
-      })}
-    </Fragment>
-  ));
+  Sites.map((e, i) => {
+    return (
+      <Fragment key={`Loop_${i}`}>
+        <h1>{e}</h1>
+        {NEWSLETTER_ITEMS.filter((c) => c.site === e).map((d, l) => {
+          return <Block key={`Block_${l}`} {...d} />;
+        })}
+      </Fragment>
+    );
+  });
 
 const page = () => {
-  const [currentUser, setCurrentUser] = useState(
-    "USER_WITH_MULTIPLE_SUBSCRIPTION"
-  );
+  const { currentUser, updateCurrentUser } = usePageStore((state) => state);
   return (
     <div>
       <h1>{currentUser}</h1>
       <select
         defaultValue={currentUser}
-        onChange={(e) => setCurrentUser(() => e.target.value)}
+        onChange={(e) => updateCurrentUser(e.target.value)}
       >
         {Object.keys(Users).map((user, l) => (
           <option key={`Option_User_${l}`} value={user}>
