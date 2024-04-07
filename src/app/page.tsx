419b7b08fff8320d1f1e73f46978227f3d2770cc
Gary FELICITE-PRUDENT <usualpro@gmail.com>
2024-04-07 11:06:55 +0200

    feature/newsletters-integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 "use server";
 
 import React from "react";
+import {
+  USER_WITH_ONE_SUBSCRIPTION,
+  USER_WITHOUT_SUBSCRIPTION,
+  USER_WITH_MULTIPLE_SUBSCRIPTION,
+} from "../mocks/user";
+
+import { NEWSLETTER_ITEMS } from "../mocks/newsletters";
+
+const Users = {
+  USER_WITH_ONE_SUBSCRIPTION,
+  USER_WITHOUT_SUBSCRIPTION,
+  USER_WITH_MULTIPLE_SUBSCRIPTION,
+};
+
+const Sites = [...new Set(NEWSLETTER_ITEMS.map((item) => item.site))];
+
+const Loop = () => Sites.map((e) => <>{e}</>);
 
 const page = () => {
-  return <div>This is a default page</div>;
+  return (
+    <div>
+      <Loop />
+    </div>
+  );
 };
 
 export default page;
