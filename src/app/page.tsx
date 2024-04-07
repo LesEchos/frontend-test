82ebe1af6d58c3140d27e997b64a12751293c25d
Gary FELICITE-PRUDENT <usualpro@gmail.com>
2024-04-07 11:37:16 +0200

    add Select and options for select user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-"use server";
+"use client";
 
-import React from "react";
+import React, { Fragment, useState } from "react";
 import {
   USER_WITH_ONE_SUBSCRIPTION,
   USER_WITHOUT_SUBSCRIPTION,
@@ -15,6 +15,15 @@ interface INewsLetter {
   description: string;
   title: string;
   site: string;
+  subscriptions: string[];
+}
+
+interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  email: string;
   subscriptions: string[];
 }
 
@@ -38,18 +47,32 @@ const Block = ({ image, title, description, subscriptions }: INewsLetter) => {
 const Sites = [...new Set(NEWSLETTER_ITEMS.map((item) => item.site))];
 
 const Loop = () =>
-  Sites.map((e) => (
-    <>
+  Sites.map((e, i) => (
+    <Fragment key={`Loop_${i}`}>
       <h1>{e}</h1>
-      {NEWSLETTER_ITEMS.filter((c) => c.site === e).map((d) => {
-        return <Block {...d} />;
+      {NEWSLETTER_ITEMS.filter((c) => c.site === e).map((d, l) => {
+        return <Block key={`Block_${l}`} {...d} />;
       })}
-    </>
+    </Fragment>
   ));
 
 const page = () => {
+  const [currentUser, setCurrentUser] = useState(
+    "USER_WITH_MULTIPLE_SUBSCRIPTION"
+  );
   return (
     <div>
+      <h1>{currentUser}</h1>
+      <select
+        defaultValue={currentUser}
+        onChange={(e) => setCurrentUser(() => e.target.value)}
+      >
+        {Object.keys(Users).map((user, l) => (
+          <option key={`Option_User_${l}`} value={user}>
+            {user}
+          </option>
+        ))}
+      </select>
       <Loop />
     </div>
   );
