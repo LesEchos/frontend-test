d637f7e1484a93d7912a1fd0203498e131b28909
Gary FELICITE-PRUDENT <usualpro@gmail.com>
2024-04-07 11:18:10 +0200

    Display pictures and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,30 @@ import {
 
 import { NEWSLETTER_ITEMS } from "../mocks/newsletters";
 
+interface INewsLetter {
+  id: string;
+  image: string;
+  description: string;
+  title: string;
+  site: string;
+  subscriptions: string[];
+}
+
 const Users = {
   USER_WITH_ONE_SUBSCRIPTION,
   USER_WITHOUT_SUBSCRIPTION,
   USER_WITH_MULTIPLE_SUBSCRIPTION,
+};
+
+const Block = ({ image, title, description, subscriptions }: INewsLetter) => {
+  return (
+    <div>
+      <img {...{ title }} src={image} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <button></button>
+    </div>
+  );
 };
 
 const Sites = [...new Set(NEWSLETTER_ITEMS.map((item) => item.site))];
@@ -22,7 +42,7 @@ const Loop = () =>
     <>
       <h1>{e}</h1>
       {NEWSLETTER_ITEMS.filter((c) => c.site === e).map((d) => {
-        return <>{JSON.stringify(d)}</>;
+        return <Block {...d} />;
       })}
     </>
   ));
